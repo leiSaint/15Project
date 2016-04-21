7eeea4024f185b64c00766a78f5b394cdb6227d1
leiSaint <leijiayi@live.cn>
2016-04-21 15:06:28 +0800

    包含了最新的库

--- src/project/allLib.py
+++ src/project/allLib.py
@@ -36,19 +36,45 @@ NtCallsTree = [
                ['NtOpenKeyEx', 'NtClose','leafRegOpenKey'],
                ['NtOpenKey', 'NtDeleteKey', 'NtClose', 'leafRegDeleteKey'],
                ['NtOpenKeyEx', 'NtDeleteValueKey', 'NtClose', 'leafDeleateKeyValue'],
-               ['NtEnumerateKey.KeyBasicInformation', 'leafEnumKey']
+               ['NtEnumerateKey.KeyBasicInformation', 'leafEnumKey'],
                ['NtCreateFile', 'leafCreateFile'],
                ['NtCreateUserProcess', 'leafCreateProcess'],
                ['NtTerminateProcess', 'leafExitProcess'],
                ['NtOpenProcess', 'NtCreateThreadEx', 'leafCreateRemoteThread'],
                ['NtOpenProcess', 'leafOpenProcess'],
-               ['NtCreateThreadEx', 'leafCreateThread']
+               ['NtCreateThreadEx', 'leafCreateThread'],
                ['NtCreateSection', 'NtMapViewOfSection', 'NtUnmapViewOfSection', 'leafCreateToolhelp32SnapShot'],
                ['NtCreateSection', 'NtMapViewOfSection', 'NtUnmapViewOfSection', 'NtClose', 'leafCreateToolhelp32SnapShot&CloseHandle'],
                ['NtCreateSection', 'NtMapViewOfSection', 'NtUnmapViewOfSection', 'NtMapViewOfSection', 'NtUnmapViewOfSection', 'NtClose', 'leafCreateToolhelp32SnapShot&Process32First&Process32Next'],
                ]
+callSet = ['NtSetValueKey', 'NtSetInformationFile.FileBasicInformation', 'NtCreateKey', 'NtOpenProcessToken', 'LeafOpenFile', 'NtOpenThreadToken', 'NtCreateUserProcess', 
+           'NtQueryInformationFile.FileDispositionInformation', 'NtWriteFile', 'NtMapViewOfSection', 'NtCreateThreadEx', 'NtDeleteKey', 
+           'NtQueryValueKey', 'NtCreateSection', 'NtOpenProcess',
+             ]
 
 #定义各个系统调用node的创建方式，使用字典来存储，包括关注的参数及该参数对应的正则表达式和要去掉的字符,数字表示从系统调用名数起的行数
-parseDict = {'NtCreateFile': [{12:'=> .*? ', 'strip': '=> '},{11:'succeed|failed'},{2:'\?\?.*?\"','strip':'\?\?\"'}],
-             'NtClose': [{1:'0x.* '}, {2:'succeed|failed'}, {3:' ', 'strip': ' '}],
-             'NtOpenFile': [{8: '=> .*? ', 'strip': '=> '}, {7:'succeed|failed'}, {2:'\?\?.*?\"','strip':'\?\?\"'}]}             
+parseDict = {'NtCreateFile': [{12:'=> .*? ', 'strip': '=> '},{11:'succeeded|failed'},{2:'\?\?.*?\"','strip':'\?\?\"'}],
+             'NtClose': [{0:'0x.*? ', 'strip': ' '}, {1:'succeeded|failed'}, {2:' ', 'strip': ' '}],
+             'NtOpenFile': [{7: '=> .*? ', 'strip': '=> '}, {6:'succeeded|failed'}, {2:'\?\?.*?\"','strip':'\?\?\"'}], 
+             'NtSetValueKey': [{0:'0x.*? '},{6:'succeeded|failed'},{1:'".*"'}],
+             'NtSetInformationFile.FileBasicInformation':[{0:'0x.*? '},{5:'succeeded|failed'},{2:'0x.*? '}],
+             'NtCreateKey': [{8:'=> .*? ', 'strip': '=> '},{7:'succeeded|failed'},{2:'".*?"'}],
+             'NtOpenProcessToken': [{5:'=> .*? ', 'strip': '=> '},{3:'succeeded|failed'},{0:'0x.*? '}],
+             'NtOpenThreadToken': [{5:'=> .*? ', 'strip': '=> '},{3:'succeeded|failed'},{0:'0x.*? '}],
+             'NtCreateUserProcess': [{12:'=> .*? ', 'strip': '=> '},{11:'succeeded|failed'},{1:'0x.*? '}],
+             'NtSetInformationFile.FileDispositionInformation': [{0:'0x.*? ', 'strip': ' '}, {5:'succeeded|failed'}, {3:'0x.*? ', 'strip': ' '}],
+             'NtWriteFile': [{0:'0x.*? '},{9:'succeeded|failed'},{5:'0x.*? '}],
+             'NtCreateThreadEx': [{12:'=> .*? ', 'strip': '=> '},{11:'succeeded|failed'},{3:'0x.*? '}],
+             'NtDeleteKey': [{0:'0x.*? '}, {1:'succeeded|failed'}, {2:'\?\?.*?\"','strip':'\?\?\"'}],
+             'NtQueryValueKey': [{0:'0x.*? '}, {6:'succeeded|failed'}, {3:'0x.*? '}],
+             'NtOpenKeyEx': [{5: '=> .*? ', 'strip': '=> '}, {4:'succeeded|failed'}, {2:'\".*?\"', 'strip':'\"'}],
+             'NtOpenKey': [{5: '=> .*? ', 'strip': '=> '}, {4:'succeeded|failed'}, {2:'\".*?\"', 'strip':'\"'}],
+             'NtDeleteValueKey': [{0:'0x.*? ', 'strip': ' '}, {2:'succeeded|failed'}, {2:'\".*?\"', 'strip':'\"'}],
+             'NtQueryDirectoryFile.FileBothDirectoryInformation':[{0:'0x.*? ', 'strip': ' ', 11:'succeeded|failed'}, {9:'\".*?\"', 'strip':'\"'}],
+             'NtUnmapViewOfSection':[{0: '=> .*? ', 'strip': '=> '}, {2:'succeeded|failed'}, {3:'0x.*? ', 'strip': ' '}],
+             'NtEnumerateKey.KeyBasicInformation': [{0:'0x.*? ', 'strip': ' '}, {6:'succeeded|failed'}, {8:'=> .*? ', 'strip': '=> '}],
+             'NtTerminateProcess': [{0: '=> .*? ', 'strip': '=> '}, {2:'succeeded|failed'}, {3:'0x.*? ', 'strip': ' '}],
+             'NtOpenProcess':[{5:'=> .*? ', 'strip': '=> '},{4:'succeeded|failed'}, {2:'name=.*?,', 'strip':'name=,'}],
+             'NtCreateSection':[{8:'=> .*? ', 'strip': '=> '}, {7:'succeeded|failed'}, {2:'\".*?\"', 'strip':'\"'}],
+             'NtMapViewOfSection':[{0:'0x.*? ', 'strip': ' '}, {10:'succeeded|failed'}, {1:'0x.*? ', 'strip': ' '}]
+             }             
